f565b441bf5ae2a1aa1f66f214b9c2b697ec62b3
maotterson <otterson.2@gmail.com>
2021-08-17 00:20:32 -0400

    add icons & reactively render skill list... todo: webpack import fonts

---
+++ components/SkillList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ul class="flex flex-row">
+      <li v-for="skill of skills" :key="skill.slug">
+        {{ skill.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    'skills'
+  ]
+  
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Skills</h1>
+    <SkillList :skills="skills" />
+  </div>
 </template>
 
 <script>
+import '../styles/icons.css';
+
 export default {
   head(){
     return {
       script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     }
-  }
+  },
+  async asyncData({$content}){
+    const skills = await $content("skill").fetch()
+
+    return {
+      skills
+    }
+  },
 }
 </script>
