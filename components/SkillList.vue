a6734e27bcff6ed72aecd0995ab0712a984e5490
maotterson <otterson.2@gmail.com>
2021-08-18 14:27:54 -0400

    add modal to display skill information

--- components/SkillList.vue
+++ components/SkillList.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
+    <SkillModal
+      :expandedSkill="expandedSkill"
+      v-if="expandedSkill" 
+      @closemodal="closeModal"
+    />
     <ul class="flex flex-col">
       <li v-for="category of categories" :key="category.slug">
         {{ category.title }}
-        <ul class="flex flex-row">
-          <li v-for="skill of category.skills" :key="skill.slug" class="border-2 bg-gray-100 rounded p-2 mr-2">
+        <ul class="flex flex-row flex-wrap justify-items-stretch">
+          <li v-for="skill of category.skills" 
+            :key="skill.slug" 
+            @click="expandSkill(skill)" 
+            :class="(skill===expandedSkill)?'bg-green-800 border-green-900 text-white':'bg-gray-100 '" 
+            class="border-2 rounded hover:bg-green-100 hover:border-green-400 p-2 mr-2 mb-2">
             <span :class="skill.icon"></span> {{skill.title}}
           </li>
         </ul>
@@ -17,8 +26,28 @@
 export default {
   props:[
     'skills','categories'
-  ]
-  
+  ],
+  data() {
+    return {
+      expandedSkill:null
+    }
+  },
+  methods:{
+    expandSkill(skill){
+      // toggle the expanded skill to whatever was clicked
+      if(!this.expandedSkill || this.expandedSkill!=skill){
+        this.expandedSkill = skill
+        return
+      }
+
+      // toggle the expanded skill off
+      this.expandedSkill = null
+
+    },
+    closeModal(){
+      this.expandedSkill=null
+    }
+  }
 }
 </script>
 
---
+++ components/SkillModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto">
+    <div class="flex items-end justify-center justify-items-start pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div @click="closeModal()" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
+      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <h1 class="text-4xl	mb-4"><span :class="expandedSkill.icon"></span> {{expandedSkill.title}}</h1>
+
+          <ul class="flex flex-row flex-wrap justify-items-stretch mb-4">
+            <li v-for="subcategory in expandedSkill.subcategories" :key="subcategory" class="rounded bg-gray-100 border-2 px-2 mr-2">
+              {{subcategory}}
+            </li>
+          </ul>
+
+          <p class="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, cum eos odio voluptas asperiores hic laborum quae itaque iure aperiam quasi nesciunt explicabo cupiditate! Corrupti atque libero fugiat quisquam aspernatur.</p>
+          
+          <h2 class="text-2xl" v-if="expandedSkill.taggedProjects">Projects</h2>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:[
+    'expandedSkill'
+  ],
+  methods:{
+    closeModal(){
+      this.$emit('closemodal')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
