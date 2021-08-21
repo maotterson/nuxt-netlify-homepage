037c61f49341c3bce9d831adbba5b34716a62a9a
maotterson <otterson.2@gmail.com>
2021-08-21 13:00:24 -0400

    flex main layout:

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="lg:px-40 bg-black">
+  <div class="bg-black">
     <div class="flex flex-col" :class="(this.$store.state.drawerShowing)?'min-h-screen':''">
-      <Topbar class="text-white bg-blue-900 md:hidden" />
+      <Topbar class="text-white bg-blue-900 lg:hidden" />
       <Drawer class="text-white bg-blue-900 flex-grow	" v-if="this.$store.state.drawerShowing"/>
     </div>
-    <div class="grid grid-cols-8" v-if="!this.$store.state.drawerShowing">
-      <Sidebar 
-        class="text-white bg-blue-900 hidden md:col-span-3 xl:col-span-2 md:block"/>
-      <nuxt class="bg-white min-h-screen col-span-8 md:col-span-5 xl:col-span-6 shadow-xl p-4 lg:p-12"></nuxt>
-      <Footer/>
+    <div class="w-full max-w-7xl mx-auto" v-if="!this.$store.state.drawerShowing">
+      <div class="lg:flex">
+        <Sidebar 
+          class="text-white bg-blue-900 hidden lg:block"/>
+        <nuxt class="bg-white min-h-screen shadow-xl p-4 lg:p-12"></nuxt>
+      </div>
     </div>
+    <Footer/>
   </div>
 </template>
 
