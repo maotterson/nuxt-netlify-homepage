0c233cbe0a3bc5acd58ff137bd86fcc892367a44
maotterson <otterson.2@gmail.com>
2021-08-24 23:46:41 -0400

    test consuming hello world function

--- pages/blog.vue
+++ pages/blog.vue
@@ -2,13 +2,21 @@
   <div>
     <div class="prose md:prose-xl">
       <h1>Blog</h1>
+      <h2>Not exactly a blog... testing consuming netlify function</h2>
+      <p>{{data}}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  async asyncData({ $axios }) {
+    const endpoint = "https://v2.maotterson.com/.netlify/functions/hello-world"
+    const data = await $axios.$get(endpoint)
+    return {
+      data
+    }
+  }
 }
 </script>
 
