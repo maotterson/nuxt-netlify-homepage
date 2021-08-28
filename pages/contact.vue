d2cdbe911428edc5def55a1b2c6cc69d8e2e0dc3
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2021-08-28 00:45:20 -0400

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="prose md:prose-xl">
+    <div class="prose md:prose-xl mt-8 md:mt-0">
       <h1>Contact Me</h1>
       <h3>Fill out the form if you would like to connect!</h3>
     </div>
