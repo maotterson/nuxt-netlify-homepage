70c82a47e9e722168e90f777977883250220470f
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2021-08-29 01:14:45 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="prose md:prose-xl mt-8 md:mt-0">
       <h1>My name is <span class="font-bold text-blue-500">Mark Otterson</span>.</h1>
-      
+      <h2>{{$colorMode.value}}</h2>
     </div>
   </div>
 </template>
