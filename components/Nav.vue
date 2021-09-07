ca468731ec49cc0427b4c7c395b964e89863155e
maotterson <otterson.2@gmail.com>
2021-09-07 11:49:41 -0400

    remove contact page for now

--- components/Nav.vue
+++ components/Nav.vue
@@ -33,10 +33,6 @@ export default {
         {
           "title":"Resume",
           "href":"/Resume/"
-        },
-        {
-          "title":"Contact",
-          "href":"/Contact/"
         }
       ]
     }
--- components/SkillModal.vue
+++ components/SkillModal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed z-10 inset-0 overflow-y-auto">
-    <div class="flex items-end justify-center justify-items-start pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <div class="flex items-end justify-center justify-items-start pt-4 px-16 pb-20 text-center sm:block sm:p-0">
       <div @click="closeModal()" class="fixed inset-0 bg-gray-500 dark:bg-black dark:bg-opacity-80 bg-opacity-75 transition-opacity"></div>
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
       <div class="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
