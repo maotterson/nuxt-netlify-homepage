e4564093ad2f4762be53e0bbb96a3e5e750049a9
maotterson <otterson.2@gmail.com>
2021-09-07 14:25:23 -0400

    add date to activity project

--- pages/mini-projects.vue
+++ pages/mini-projects.vue
@@ -13,6 +13,7 @@
       </div>
 
       <h2>Strava Activity Calendar</h2>
+      <h5>(September 2021)</h5>
       <p>I created a calendar displaying all of monthly activities logged to Strava.</p>
       <div class="flex-row flex justify-center">
         <div class="flex flex-row items-center">
