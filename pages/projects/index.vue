565d0aefa2e5df89a12ea0933c237a530d07c373
maotterson <otterson.2@gmail.com>
2021-09-11 14:22:50 -0400

    add thumbnails to project page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -4,8 +4,12 @@
       <h1 class="">Projects</h1>
       <h3>A selection of projects I have worked on.</h3>
       <div class="flex flex-col">
-        <div v-for="project of projects" :key="project.slug" class="flex flex-row p-2">
-          
+        <div v-for="project of projects" :key="project.slug" class="flex flex-row items-center  p-2">
+          <div class="flex-none sm:mr-8 mr-4">
+            <NuxtLink :to="project.slug">
+              <img class="h-16 sm:h-24" :src="'/thumb/'+project.thumb"/>
+            </NuxtLink>
+          </div>
           <div>
             <h3><NuxtLink :to="project.slug">{{ project.title }}</NuxtLink></h3>
             <p>{{project.description}}</p>
