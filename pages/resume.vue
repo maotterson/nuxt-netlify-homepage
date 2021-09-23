0eee5b808da8526b33adc98def24c74ace285690
maotterson <otterson.2@gmail.com>
2021-09-22 23:14:10 -0400

    add pdf resume

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,7 +4,7 @@
       <h1>Resume</h1>
       <h3>A browser-friendly version of my resume.</h3>
       <div class="flex flex-row prose-buttons">
-        <a href="#"><div class="hover:bg-blue-800 bg-blue-600 rounded-xl border-2 px-4 p-2 "><span class="icon-file-pdf mr-2"></span>PDF Version</div></a>
+        <a href="/resume.pdf"><div class="hover:bg-blue-800 bg-blue-600 rounded-xl border-2 px-4 p-2 "><span class="icon-file-pdf mr-2"></span>PDF Version</div></a>
       </div>
       <nuxt-content :document="resume" />
     </div>
