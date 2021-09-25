a907765bd0ba5fc4f5cfc8ccc40bddbfd353d96e
maotterson <otterson.2@gmail.com>
2021-09-24 23:33:28 -0400

    fix resume errant comma, edit metadata

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,7 +4,7 @@
       <h1>Resume</h1>
       <h3>A browser-friendly version of my resume.</h3>
       <div class="flex flex-row prose-buttons">
-        <a href="/resume.pdf"><div class="hover:bg-blue-800 bg-blue-600 rounded-xl border-2 px-4 p-2 "><span class="icon-file-pdf mr-2"></span>PDF Version</div></a>
+        <a href="/Mark%20Otterson%20-%20Resume.pdf"><div class="hover:bg-blue-800 bg-blue-600 rounded-xl border-2 px-4 p-2 "><span class="icon-file-pdf mr-2"></span>PDF Version</div></a>
       </div>
       <nuxt-content :document="resume" />
     </div>
