56b4075ce64fdced518216346bcdbae3b0eb7f94
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2022-05-17 02:57:16 -0400

    Update index.vue
    Fix a missing "

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <h1><span class="font-bold">I'm <span class="text-blue-500 font-extrabold	">Mark Otterson</span>.</span></h1>
       <h2><span class="font-bold">I am an aspiring <span class="text-yellow-500 font-extrabold	">ASP.NET Developer</span>.</span></h2>
       <h2><span class="font-bold">I am from <span class="text-red-500 font-extrabold	">Cleveland, Ohio</span> and attended <span class="text-red-500 font-extrabold	">The Ohio State University</span>.</span></h2>
-      <h2><span class="font-bold">I am currently learning more about <span class="text-green-500 font-extrabold	">design architecture</span>, <span class="text-green-500 font-extrabold	">back-end engineering</span>, and <span class="text-green-500 font-extrabold>container orchestration</span>.</span></h2>
+      <h2><span class="font-bold">I am currently learning more about <span class="text-green-500 font-extrabold	">design architecture</span>, <span class="text-green-500 font-extrabold	">backend engineering</span>, and <span class="text-green-500 font-extrabold">container orchestration</span>.</span></h2>
     </div>
   </div>
 </template>
