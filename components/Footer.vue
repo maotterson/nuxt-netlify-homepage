24410c0ea43436adfbf98bb003af6278bbf6d507
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2022-05-17 02:54:15 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col  bg-gray-800 text-white">
     <SocialLinks class="justify-center pt-8" />
     <div class="flex flex-row items-center justify-center py-4">
-      <h1><span>© 2021</span> <span class="font-bold">Mark Otterson</span></h1>
+      <h1><span>© 2022</span> <span class="font-bold">Mark Otterson</span></h1>
     </div>
   </div>
 </template>
