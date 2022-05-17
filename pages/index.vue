53db799d6447d0d7369433498d9da42408e22b10
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2022-05-17 02:29:58 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="dark:prose-dark prose	md:prose-lg lg:prose-xl mt-8 md:mt-0">
       <h1><span class="font-bold">I'm <span class="text-blue-500 font-extrabold	">Mark Otterson</span>.</span></h1>
-      <h2><span class="font-bold">I am a <span class="text-yellow-500 font-extrabold	">web developer</span> and <span class="text-yellow-500 font-extrabold">professional tutor</span>.</span></h2>
+      <h2><span class="font-bold">I am an aspiring <span class="text-yellow-500 font-extrabold	">ASP.NET Developer</span>.</span></h2>
       <h2><span class="font-bold">I am from <span class="text-red-500 font-extrabold	">Cleveland, Ohio</span> and attended <span class="text-red-500 font-extrabold	">The Ohio State University</span>.</span></h2>
     </div>
   </div>
