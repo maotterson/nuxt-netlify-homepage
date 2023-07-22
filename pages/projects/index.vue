b0ea15155c4e07d52d3d08a3e51adf22b28f5155
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2023-07-22 02:03:02 -0400

    Update projects page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,9 +2,9 @@
   <div>
     <div class="dark:prose-dark prose md:prose-xl mt-8 md:mt-0 mb-8">
       <h1 class="">Projects</h1>
-      <h3>A selection of projects I have worked on.</h3>
+      <h3>A selection of personal projects I have worked on.</h3>
       <div class="flex flex-col divide-y divide-grey-800">
-        <div v-for="project of projects" :key="project.priority" class="flex flex-row items-center  pb-8 p-2 ">
+        <div v-for="project of projects" :key="project.priority" class="flex flex-row items-center mt-0 pt-0 p-4 bg-gray-100 dark:bg-gray-700 shadow rounded mb-4">
           <div>
             <h3><NuxtLink :to="project.slug">{{ project.title }}</NuxtLink></h3>
             <h6 class="font-bold text-red-500">{{project.date}}</h6>
