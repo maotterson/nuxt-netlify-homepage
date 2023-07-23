5a30830a7f6816b9e3924eba475d2cab55bf6a69
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2023-07-23 13:49:09 -0400

    Update index.vue

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,7 +7,7 @@
         <div v-for="project of projects" :key="project.priority" class="flex flex-row items-center mt-0 pt-0 p-4 bg-gray-100 dark:bg-gray-700 shadow rounded mb-4">
           <div>
             <h3><NuxtLink :to="project.slug">{{ project.title }}</NuxtLink></h3>
-            <h6 class="font-bold text-red-500">{{project.date}}</h6>
+            <h6 class="font-bold text-grey-600">{{project.date}}</h6>
             <p>{{project.description}}</p>
           </div>
         </div>
