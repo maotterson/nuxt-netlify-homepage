c9fb8fc3d2f6d8263d76f2b84a1f9512d0ae19f1
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2023-07-23 23:44:25 -0400

    Add contrast to projects overview

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -6,8 +6,8 @@
       <div class="flex flex-col divide-y divide-grey-800">
         <div v-for="project of projects" :key="project.priority" class="flex flex-row items-center mt-0 pt-0 p-4 bg-gray-100 dark:bg-gray-700 shadow rounded mb-4">
           <div>
-            <h3><NuxtLink :to="project.slug">{{ project.title }}</NuxtLink></h3>
-            <h6 class="font-bold text-grey-600">{{project.date}}</h6>
+            <h1><NuxtLink :to="project.slug">{{ project.title }}</NuxtLink></h1>
+            <h6 class="mt-2 font-bold text-grey-500">{{project.date}}</h6>
             <p>{{project.description}}</p>
           </div>
         </div>
