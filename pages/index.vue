813f955b63e8c2289cdd6d1e9a7741c5922b4123
Mark Otterson <48816461+maotterson@users.noreply.github.com>
2025-08-14 15:06:17 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="dark:prose-dark prose	md:prose-lg lg:prose-xl mt-8 md:mt-0">
-      <h1><span class="font-bold">👋 I'm <span class="text-blue-500 font-extrabold	">Mark Otterson</span>.</span></h1>
-      <h2><span class="font-bold">I am a developer specializing in the <span class="text-yellow-500 font-extrabold	">ASP.NET ecosystem</span>.</span></h2>
-      <h2><span class="font-bold">I am from <span class="text-red-500 font-extrabold	">Cleveland, Ohio</span> and attended <span class="text-red-500 font-extrabold	">The Ohio State University</span>.</span></h2>
-      <h2><span class="font-bold">I am currently learning more about <span class="text-green-500 font-extrabold	">design architecture</span>, <span class="text-green-500 font-extrabold	">backend engineering</span>, and <span class="text-green-500 font-extrabold">container orchestration</span>.</span></h2>
-    </div>
+    <div class="dark:prose-dark prose md:prose-lg lg:prose-xl mt-8 md:mt-0">
+  <h1><span class="font-bold">👋 I'm <span class="text-blue-500 font-extrabold">Mark Otterson</span>.</span></h1>
+  <h2><span class="font-bold">I build modern web applications using <span class="text-yellow-500 font-extrabold">Laravel</span> on the backend and <span class="text-green-500 font-extrabold">Vue.js</span> on the frontend.</span></h2>
+  <h2><span class="font-bold">Based in <span class="text-red-500 font-extrabold">Cleveland, Ohio</span>. I'm a proud graduate of <span class="text-red-500 font-extrabold">The Ohio State University</span>.</span></h2>
+</div>
+
   </div>
 </template>
 
